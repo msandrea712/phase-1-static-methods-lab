5becd6801a45d66012d3224ecf848acefadf6563
Andrea Cox <msandrea712@yahoo.com>
2023-06-03 12:28:44 -0400

    static methods lab

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
+class StringUtils {
+  static capitalize(str) {
+    if (str === null || str.length === 0) {
+      return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    if (str === null || str.length === 0) {
+      return str;
+    }
+    return str.replace(/[^a-zA-Z0-9-' ]/g, "");
+  }
+
+  static titleize(str) {
+    if (str === null || str.length === 0) {
+      return str;
+    }
+    const words = str.toLowerCase().split(" ");
+    const excludedWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const result = [];
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (i === 0 || !excludedWords.includes(word)) {
+        result.push(StringUtils.capitalize(word));
+      } else {
+        result.push(word);
+      }
+    }
+    return result.join(" ");
+  }
+}
+
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return StringUtils.capitalize(str);
+  }
+
+  static sanitize(str) {
+    return StringUtils.sanitize(str);
+  }
+
+  static titleize(str) {
+    return StringUtils.titleize(str);
+  }
 }
